5cbc8219cbdec9d1d9b38b0ed33ac85aea42c0f0
Hugomatiasdevv <contact@hugo-ofm.fr>
2025-04-02 12:38:13 +0200

    Add files via upload

---
+++ database_light.js
@@ -0,0 +1,43 @@
+
+const database = {
+  europeenne: [
+    { prenom: "Emma", nom: "Morel", handle: "emma.mrl" },
+    { prenom: "Chloé", nom: "Dubois", handle: "chloe.dbs" },
+    { prenom: "Camille", nom: "Lemoine", handle: "camille.lmn" }
+  ],
+  asiatique: [
+    { prenom: "Hana", nom: "Sato", handle: "hana.sto" },
+    { prenom: "Yumi", nom: "Lee", handle: "yumi.lee" },
+    { prenom: "Mei", nom: "Zhang", handle: "mei.zhg" }
+  ],
+  arabe: [
+    { prenom: "Layla", nom: "Hassan", handle: "layla.hsn" },
+    { prenom: "Jana", nom: "Alami", handle: "jana.alm" },
+    { prenom: "Maya", nom: "Khalil", handle: "maya.khl" }
+  ],
+  pays_est: [
+    { prenom: "Anya", nom: "Petrova", handle: "anya.ptr" },
+    { prenom: "Irina", nom: "Smirnova", handle: "irina.smv" },
+    { prenom: "Katia", nom: "Ivanova", handle: "katia.ivn" }
+  ],
+  moyen_orient: [
+    { prenom: "Dina", nom: "Salem", handle: "dina.slm" },
+    { prenom: "Noor", nom: "Zahran", handle: "noor.zhr" },
+    { prenom: "Yara", nom: "Tariq", handle: "yara.trq" }
+  ],
+  africaine: [
+    { prenom: "Aminata", nom: "Diop", handle: "aminata.dpp" },
+    { prenom: "Fatou", nom: "Sow", handle: "fatou.sow" },
+    { prenom: "Zina", nom: "Koulibaly", handle: "zina.klb" }
+  ],
+  latine: [
+    { prenom: "Sofia", nom: "Ramirez", handle: "sofia.rmr" },
+    { prenom: "Valentina", nom: "Morales", handle: "valentina.mrl" },
+    { prenom: "Isabella", nom: "Castro", handle: "isabella.cst" }
+  ],
+  metissee: [
+    { prenom: "Naomi", nom: "Lee", handle: "naomi.lee" },
+    { prenom: "Lina", nom: "Smith", handle: "lina.smt" },
+    { prenom: "Zara", nom: "Martin", handle: "zara.mrt" }
+  ]
+};
